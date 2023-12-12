684536307cf32426427a757d3824579f6631d37e
garimiya <garimiya@gmail.com>
2023-12-12 16:25:07 +0900

    add hello method

--- src/com/intheeast/Dao/UserDao.java
+++ src/com/intheeast/Dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.intheeast.Dao;
 
-public class garim {
+public class UserDao {
 
 	public static void main(String[] args) {
 		System.out.print("iss53");
+		
+		System.put.print("Hello World");
 
 	}
 
